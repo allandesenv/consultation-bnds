565b5cad11932d581314b59933572edbc34dd352
allandesenv <allandd8@gmail.com>
2022-12-16 02:12:20 -0300

    Initial Commit

---
+++ spring-boot-et3/booting-web-v2/src/main/java/com/rosa/tes/bootingwebv2/BootingWebV2Application.java
@@ -0,0 +1,13 @@
+package com.rosa.tes.bootingwebv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootingWebV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootingWebV2Application.class, args);
+	}
+
+}
